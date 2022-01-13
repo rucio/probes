98d332dc0d352bac029d8e7636c4002a7a630dd0
Eric Vaandering <ericvaandering@gmail.com>
2022-01-13 09:22:31 -0600

    Add a prefix and optional labels to prometheus metrics

---
+++ cms/utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright European Organization for Nuclear Research (CERN) 2020
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Eric Vaandering, <ewv@fnal.gov>, 2022
+
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'common', 'utils')))
+
+import common
